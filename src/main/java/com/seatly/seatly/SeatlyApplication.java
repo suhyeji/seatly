add021fa6313ceecfa19fdf3b1ec7f5a0d3d1b8c
suhyeji@naver.com <suhyeji@naver.com>
2025-12-15 21:22:46 +0900

    최초 프로젝트 생성

---
+++ src/main/java/com/seatly/seatly/SeatlyApplication.java
@@ -0,0 +1,13 @@
+package com.seatly.seatly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seatly/seatly/SeatlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatly.seatly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
