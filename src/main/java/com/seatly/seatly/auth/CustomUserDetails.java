bcb6682dbb4d9d9f68a6bc7759b3f3a136fb45c4
서예지 <suhyeji@naver.com>
2025-12-30 17:35:46 +0900

    CustomUserDetails에 email 추가

--- src/main/java/com/seatly/seatly/auth/CustomUserDetails.java
+++ src/main/java/com/seatly/seatly/auth/CustomUserDetails.java
@@ -18,11 +18,13 @@ public class CustomUserDetails implements UserDetails {
 
   private final Long id;
   private final String username;
+  private final String email;
   private final UserRole role;
 
   public CustomUserDetails(User user) {
     this.id = user.getId();
     this.username = user.getName();
+    this.email = user.getEmail();
     this.role = user.getRole();
   }
 
