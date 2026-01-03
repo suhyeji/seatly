1a0bf0fb08e5c486657b141171250ae135cbc0b4
jiyoon <chlwldbs8136@naver.com>
2026-01-03 13:01:38 +0900

    Facility Enum 변경

--- src/main/java/com/seatly/seatly/domain/enums/Facility.java
+++ src/main/java/com/seatly/seatly/domain/enums/Facility.java
@@ -1,36 +1,25 @@
 package com.seatly.seatly.domain.enums;
 
-import java.util.Arrays;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Facility {
-  WIFI("WIFI"),
-  PRINTER("PRINTER"),
-  OUTLET("OUTLET"),
-  OPEN_24H("OPEN_24H"),
-  CAFE("CAFE"),
-  MEETING_ROOM("MEETING_ROOM"),
-  LOCKER("LOCKER"),
-  AIR_CONDITIONING("AIR_CONDITIONING");
-
-  private final String value;
-
-  Facility(String value) {
-    this.value = value;
-  }
+  WIFI,
+  PRINTER,
+  OUTLET,
+  OPEN_24H,
+  CAFE,
+  MEETING_ROOM,
+  LOCKER,
+  AIR_CONDITIONING;
 
   @JsonValue
   public String getValue() {
-    return value;
+    return this.name();
   }
 
   @JsonCreator
   public static Facility from(String value) {
-    return Arrays.stream(values())
-        .filter(v -> v.value.equals(value))
-        .findFirst()
-        .orElseThrow();
+    return Facility.valueOf(value);
   }
 }
