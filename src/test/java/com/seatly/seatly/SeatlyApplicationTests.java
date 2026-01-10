473014113a36d1c9011b9329c85826481f2f9063
Jiyoon <chlwldbs8136@naver.com>
2026-01-10 18:03:55 +0900

    DB 암호화 설정

--- src/test/java/com/seatly/seatly/SeatlyApplicationTests.java
+++ src/test/java/com/seatly/seatly/SeatlyApplicationTests.java
@@ -1,13 +1,26 @@
 package com.seatly.seatly;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SeatlyApplicationTests {
 
+	@Autowired
+	private StringEncryptor encryptor;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void encrypt() {
+		String plain = "yejiyoon813";
+		String encrypted = encryptor.encrypt(plain);
+
+		System.out.println("ENC(" + encrypted + ")");
 	}
 
 }
