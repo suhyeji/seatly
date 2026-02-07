5404f0b1f5949cf4aa4a580cebfcd3e4be2d4f1d
서예지 <suhyeji@naver.com>
2026-02-07 14:24:07 +0900

    함수 삭제 복원

--- src/main/java/com/seatly/seatly/service/UserService.java
+++ src/main/java/com/seatly/seatly/service/UserService.java
@@ -3,9 +3,12 @@ package com.seatly.seatly.service;
 import java.util.List;
 
 import org.springframework.data.util.Pair;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.seatly.seatly.domain.User;
 import com.seatly.seatly.domain.enums.UserCafeLinkType;
@@ -13,6 +16,8 @@ import com.seatly.seatly.dto.TimePass;
 import com.seatly.seatly.dto.login.LoginRequest;
 import com.seatly.seatly.dto.session.SessionInfo;
 import com.seatly.seatly.dto.user.UserInfo;
+import com.seatly.seatly.dto.user.UserPasswordPut;
+import com.seatly.seatly.dto.user.UserPatch;
 import com.seatly.seatly.dto.user.UserPost;
 import com.seatly.seatly.dto.user.UserTimePassInfo;
 import com.seatly.seatly.store.SessionStoreService;
@@ -66,6 +71,23 @@ public class UserService {
 
   public List<TimePass> getTimePasses(Long id) {
     return userTimePassStoreService.findAllByUserId(id);
+  }
+
+  public void updateUserInfo(Long id, UserPatch userPatch) {
+    User user = userStoreService.findByIdOrThrow(id);
+    userStoreService.save(userPatch.patch(user));
+  }
+
+  @Transactional
+  public void updatePassword(Long id, UserPasswordPut passwordPut) {
+    User user = userStoreService.findByIdOrThrow(id);
+    if (!passwordEncoder.matches(passwordPut.getCurrentPassword(), user.getPassword())) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST,
+          "현재 비밀번호가 일치하지 않습니다.");
+    }
+    user.setPassword(passwordEncoder.encode(passwordPut.getNewPassword()));
+    userStoreService.save(user);
   }
 
   private void setDto(UserInfo result, User user) {
