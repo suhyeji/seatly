f3c650119b82ca3afba416de8c21b1d16bbd468d
서예지 <suhyeji@naver.com>
2026-02-12 18:34:09 +0900

    RedisConfig value stringSerializer 추가

--- src/main/java/com/seatly/seatly/global/RedisConfig.java
+++ src/main/java/com/seatly/seatly/global/RedisConfig.java
@@ -23,7 +23,12 @@ public class RedisConfig {
 
     RedisTemplate<String, String> template = new RedisTemplate<>();
     template.setConnectionFactory(factory);
-    template.setKeySerializer(new StringRedisSerializer());
+
+    StringRedisSerializer stringSerializer = new StringRedisSerializer();
+    template.setKeySerializer(stringSerializer);
+    template.setValueSerializer(stringSerializer);
+    template.setHashKeySerializer(stringSerializer);
+    template.setHashValueSerializer(stringSerializer);
 
     return template;
   }
